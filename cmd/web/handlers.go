0d8693e3ab36456381bb3df5d72e057d538e75b5
MehdiEidi <mehdiadq@gmail.com>
2021-08-30 20:47:51 +0430

    Database added, also insert and get methods updated accordingly

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -50,5 +50,15 @@ func (app *application) createSnippetHandler(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	w.Write([]byte("create a new snippet..."))
+	title := "0 snailsss"
+	content := "O snail\\nClimb Mount Fuji,\\nBut slowly, slowly!\\n\\n– Kobayashi Issa"
+	expires := "7"
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
 }
