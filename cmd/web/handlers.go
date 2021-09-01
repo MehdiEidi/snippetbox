d1e3fccdcf8c2d613a89190723ab1d4fd2178a11
MehdiEidi <mehdiadq@gmail.com>
2021-09-01 14:15:33 +0430

    pat router added

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -8,12 +8,7 @@ import (
 	"github.com/MehdiEidi/snippetbox/pkg/models"
 )
 
-func (app *application) homeHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		app.notFound(w)
-		return
-	}
-	
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	s, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
@@ -26,7 +21,7 @@ func (app *application) homeHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (app *application) showSnippetHandler(w http.ResponseWriter, r *http.Request) {
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
@@ -47,7 +42,7 @@ func (app *application) showSnippetHandler(w http.ResponseWriter, r *http.Reques
 	})
 }
 
-func (app *application) createSnippetHandler(w http.ResponseWriter, r *http.Request) {
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		w.Header().Set("Allow", "POST")
 		app.clientError(w, http.StatusMethodNotAllowed)
